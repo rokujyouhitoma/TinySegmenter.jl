2dcf147821870be9e36c22dc105d1fb9ee7adb06
rokujyouhitoma <rokujyouhitomajp@gmail.com>
2015-10-22 19:47:36 +0900

    Add annotate=True

--- benchmark/cythonize/setup.py
+++ benchmark/cythonize/setup.py
@@ -1,8 +1,11 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+from Cython.Build import cythonize
 
-ext_modules = [Extension("tinysegmenter", ["tinysegmenter.pyx"])]
+ext_modules = cythonize([Extension("tinysegmenter", ["tinysegmenter.pyx"])],
+#                        annotate=True
+                        )
 
 setup(
   name = 'Tiny Segmenter',
